ec98edcbf10ca3743d195a54f3645a0ceafec6dc
dan-frank <dan.frank.lucas@gmail.com>
2020-03-06 18:36:46 +0000

    Set up intersection java file

---
+++ easy/IntersectionOfArraysII.java
@@ -0,0 +1,14 @@
+// QUESTION
+// Given two arrays, write a function to compute their intersection.
+
+import java.util.Arrays;
+
+class IntersectionOfArraysII {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(intersect(new int[]{1,2,2,1}, new int[]{2,2})));
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        return new int[]{2,2};
+    }
+}
