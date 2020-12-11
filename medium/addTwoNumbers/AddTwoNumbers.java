6bddb614d25c52d79192460c5a24d1708e5faa9f
dan-frank <dan.frank.lucas@gmail.com>
2020-12-11 18:30:40 +0000

    Set up print method to view ListNode

--- medium/addTwoNumbers/AddTwoNumbers.java
+++ medium/addTwoNumbers/AddTwoNumbers.java
@@ -1,6 +1,26 @@
 class AddTwoNumbers {
     public static void main(String[] args) {
         System.out.println("test");
+        
+        ListNode l1 = new ListNode(3);
+        l1 = new ListNode(4, l1);
+        l1 = new ListNode(2, l1);
+
+        ListNode l2 = new ListNode(4);
+        l2 = new ListNode(6, l2);
+        l2 = new ListNode(5, l2);
+
+        printList(l1);
+        System.out.println("");
+        printList(l2);
+        System.out.println("");
+    }
+
+    public static void printList(ListNode root) {
+        if (root.next != null) {
+            printList(root.next);
+        }
+        System.out.print(root.val);
     }
 }
 
