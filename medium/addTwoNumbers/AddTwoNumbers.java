1759d581721f8f87eaaedcd16ea4a23ceb9f9566
dan-frank <dan.frank.lucas@gmail.com>
2020-12-11 19:20:30 +0000

    Successully create new node if a carry over remains after last calculation

--- medium/addTwoNumbers/AddTwoNumbers.java
+++ medium/addTwoNumbers/AddTwoNumbers.java
@@ -6,14 +6,14 @@ class AddTwoNumbers {
         l1 = new ListNode(4, l1);
         l1 = new ListNode(2, l1);
 
-        ListNode l2 = new ListNode(4);
+        ListNode l2 = new ListNode(8);
         l2 = new ListNode(6, l2);
         l2 = new ListNode(5, l2);
 
         printList(l1);
-        System.out.println("");
+        System.out.println("+");
         printList(l2);
-        System.out.println("");
+        System.out.println("=");
 
         ListNode l3 = addTwoNumbers(l1, l2);
         printList(l3);
@@ -24,17 +24,21 @@ class AddTwoNumbers {
         if (root.next != null) {
             printList(root.next);
         }
-        System.out.print(root.val);
+        System.out.print(root.val + "->");
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         l1.val += l2.val;
-        if (l1.next != null && l2.next != null)
-            addTwoNumbers(l1.next, l2.next);
         if (l1.val > 9) {
             l1.val -= 10;
-            l1.next.val += 1;
+            if (l1.next != null) {
+                l1.next.val += 1;
+            } else {
+                l1.next = new ListNode(1);
+            }
         }
+        if (l1.next != null && l2.next != null)
+            addTwoNumbers(l1.next, l2.next);
         return l1;
     }
 }
