e594f1ce530acf86349630b46285a5219ba45339
dan-frank <dan.frank.lucas@gmail.com>
2020-12-11 12:40:53 +0000

    Added ListNode class from Q

---
+++ medium/addTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,21 @@
+class AddTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode() {}
+    
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
