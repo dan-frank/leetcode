c2757c53c840917ce3e744f319095e6fec4bca29
dan-frank <dan.frank.lucas@gmail.com>
2020-12-13 20:23:02 +0000

    Init java class to work on CountGoodTriplets

---
+++ easy/countGoodTriplets/CountGoodTriplets.java
@@ -0,0 +1,9 @@
+class CountGoodTriplets {
+    public static void main(String[] args) {
+        System.out.println("init");
+    }
+
+    public static int countGoodTriplets(int[] arr, int a, int b, int c) {
+        return 0;
+    }
+}
