7edc21c22eb9ac989c09deecc118f646202531a3
dan-frank <dan.frank.lucas@gmail.com>
2022-10-26 15:57:15 +0100

    Added more functional solution to RemoveDuplicatesFromSortedArray problem
    Although fails to resolve problem

--- src/main/scala/easy/RemoveDuplicatesFromSortedArraySolution.scala
+++ src/main/scala/easy/RemoveDuplicatesFromSortedArraySolution.scala
@@ -18,4 +18,17 @@ object RemoveDuplicatesFromSortedArraySolution {
         solve(count + 1, lastPos + 1)
     if nums.length < 2 then nums.length
     else solve() + 1
+
+    /** NOTE [FAILURE]: `calculteLinear` is more performant for problem, but
+      * this solution is more functional and nicer to read. However, doesn't
+      * remove the elements 'in-place' like asked for by the problem and thus
+      * fails.
+      */
+  def calculteDropWhile(nums: Array[Int]): Int =
+    def solve(vals: Array[Int]): Array[Int] =
+      vals.headOption match
+        case Some(head) => head +: solve(vals.tail.dropWhile(n => n == head))
+        case None       => Array.empty[Int]
+    if nums.length < 2 then nums.length
+    else solve(nums).length
 }
