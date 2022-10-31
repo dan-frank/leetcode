7594e10fc0e03e2e81890200980f467182f39b31
dan-frank <dan.frank.lucas@gmail.com>
2022-10-31 09:40:21 +0000

    Wrote Sqrt(x) test cases

--- src/test/scala/easy/SqrtXSuite.scala
+++ src/test/scala/easy/SqrtXSuite.scala
@@ -3,9 +3,69 @@ package easy
 import easy.{SqrtXSolution => Solution}
 
 class SqrtXSuite extends munit.FunSuite {
-  test("Expect solution [0,1] from [1,2,3,4] with target 3") {
-    val obtained = Solution(Array(1, 2, 3, 4), 3)
-    val expected = Array(0, 1)
-    assertEquals(obtained.toSeq, expected.toSeq)
+  test("Expect solution 0 from 0") {
+    val obtained = Solution(0)
+    val expected = 0
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 1 from 1") {
+    val obtained = Solution(1)
+    val expected = 1
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 1 from 3") {
+    val obtained = Solution(3)
+    val expected = 1
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 2 from 4") {
+    val obtained = Solution(4)
+    val expected = 2
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 2 from 5") {
+    val obtained = Solution(5)
+    val expected = 2
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 2 from 6") {
+    val obtained = Solution(6)
+    val expected = 2
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 2 from 7") {
+    val obtained = Solution(7)
+    val expected = 2
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 2 from 8") {
+    val obtained = Solution(8)
+    val expected = 2
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 3 from 9") {
+    val obtained = Solution(9)
+    val expected = 3
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 73 from 5426") {
+    val obtained = Solution(5426)
+    val expected = 73
+    assertEquals(obtained, expected)
+  }
+
+  test("Expect solution 46340 from 2147483647") {
+    val obtained = Solution(2147483647)
+    val expected = 46340
+    assertEquals(obtained, expected)
   }
 }
