fae2afa28627ebe0f2eb73bae32af23c1ae050a7
dan-frank <dan.frank.lucas@gmail.com>
2022-10-31 12:37:52 +0000

    Improved SqrtX testing with ScalaCheckSuite

--- src/test/scala/easy/SqrtXSuite.scala
+++ src/test/scala/easy/SqrtXSuite.scala
@@ -1,66 +1,25 @@
 package easy
 
 import easy.{SqrtXSolution => Solution}
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Gen
+import scala.math.sqrt
 
-class SqrtXSuite extends munit.FunSuite {
-  test("Expect solution 0 from 0") {
-    val obtained = Solution(0)
-    val expected = 0
-    assertEquals(obtained, expected)
-  }
+class SqrtXSuite extends munit.ScalaCheckSuite {
 
-  test("Expect solution 1 from 1") {
-    val obtained = Solution(1)
-    val expected = 1
-    assertEquals(obtained, expected)
-  }
+  override def scalaCheckTestParameters =
+    super.scalaCheckTestParameters
+      .withMinSuccessfulTests(2000)
+      .withMaxDiscardRatio(10)
 
-  test("Expect solution 1 from 3") {
-    val obtained = Solution(3)
-    val expected = 1
-    assertEquals(obtained, expected)
-  }
+  val sqrtGen: Gen[Int] = Gen.choose(0, Int.MaxValue)
 
-  test("Expect solution 2 from 4") {
-    val obtained = Solution(4)
-    val expected = 2
-    assertEquals(obtained, expected)
-  }
-
-  test("Expect solution 2 from 5") {
-    val obtained = Solution(5)
-    val expected = 2
-    assertEquals(obtained, expected)
-  }
-
-  test("Expect solution 2 from 6") {
-    val obtained = Solution(6)
-    val expected = 2
-    assertEquals(obtained, expected)
-  }
-
-  test("Expect solution 2 from 7") {
-    val obtained = Solution(7)
-    val expected = 2
-    assertEquals(obtained, expected)
-  }
-
-  test("Expect solution 2 from 8") {
-    val obtained = Solution(8)
-    val expected = 2
-    assertEquals(obtained, expected)
-  }
-
-  test("Expect solution 3 from 9") {
-    val obtained = Solution(9)
-    val expected = 3
-    assertEquals(obtained, expected)
-  }
-
-  test("Expect solution 73 from 5426") {
-    val obtained = Solution(5426)
-    val expected = 73
-    assertEquals(obtained, expected)
+  property("Expect floored Int of squared root from solution") {
+    forAll(sqrtGen) { (n: Int) =>
+      val obtained = Solution(n)
+      val expected = sqrt(n).floor.toInt
+      assertEquals(obtained, expected)
+    }
   }
 
   test("Expect solution 46340 from 2147483647") {
