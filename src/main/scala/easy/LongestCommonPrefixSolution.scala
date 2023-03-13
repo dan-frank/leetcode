c04481f2085e27dd50a11d853cf6eb38277190de
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-03-13 15:04:41 +0000

    Resolve wrong questiong saved for easy/LongestCommonPrefix solution

--- src/main/scala/easy/LongestCommonPrefixSolution.scala
+++ src/main/scala/easy/LongestCommonPrefixSolution.scala
@@ -1,7 +1,9 @@
 package easy
 
-/** QUESTION: Given a string `s`, find the length of the longest substring
-  * without repeating characters.
+/** QUESTION: Write a function to find the longest common prefix string
+  * amongst an array of strings.
+  *
+  * If there is no common prefix, return an empty string "".
   */
 object LongestCommonPrefixSolution {
   def apply(strs: Array[String]): String = longestCommonPrefix(strs)
